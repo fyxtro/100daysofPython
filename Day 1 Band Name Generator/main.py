b49ecd1705c1d0c1f009dc1af56272ebba824596
F1xtro <quintenkapaan@gmail.com>
2022-01-19 11:25:28 +0100

    First Day Added

---
+++ Day 1 Band Name Generator/main.py
@@ -0,0 +1,4 @@
+print("This program will generate your band name")
+city = input("Which city do you live in?\n")
+name = input("What is your pet's name?\n")
+print("Your band name is: " + city + " " + name)
